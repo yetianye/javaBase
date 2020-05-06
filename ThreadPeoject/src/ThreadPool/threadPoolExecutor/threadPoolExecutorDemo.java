4104f9f45d1f729002356aaa3f95f785f77e94df
ye <642416071@qq.com>
2020-05-06 14:28:22 +0800

    阿大撒

--- ThreadPeoject/src/ThreadPool/threadPoolExecutor/threadPoolExecutorDemo.java
+++ ThreadPeoject/src/ThreadPool/threadPoolExecutor/threadPoolExecutorDemo.java
@@ -16,6 +16,7 @@ public class threadPoolExecutorDemo {
                 System.out.println("线程池中线程数目："+threadPoolExecutor.getPoolSize()+"，队列中等待执行的任务数目："+
                         threadPoolExecutor.getQueue().size()+"，已执行完的任务数目："+threadPoolExecutor.getCompletedTaskCount());
             }
+
     }
 }
 
