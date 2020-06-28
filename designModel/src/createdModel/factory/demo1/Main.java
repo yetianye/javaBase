f8db79232b0d915abff1edfe1b66f95e23371d5c
ye <642416071@qq.com>
2020-06-28 19:03:36 +0800

    工厂模式

---
+++ designModel/src/createdModel/factory/demo1/Main.java
@@ -0,0 +1,25 @@
+package createdModel.factory.demo1;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @Author: yetianye
+ * @CreateTime: 2020-06-27 13:23
+ * 静态工厂模式
+ * 简化的使用静态方法创建产品的方式称为静态工厂方法（Static Factory Method）
+ */
+public class Main {
+    public static void main(String[] args) {
+        //静态工厂模式
+        Integer.valueOf(123);
+
+
+    }
+}
+
+class LocalDateFactory {
+    public static LocalDate formatInt(int yyyyMMdd){
+        return LocalDate.of(yyyyMMdd/10000,yyyyMMdd/100%100,yyyyMMdd%100);
+    }
+}
