4b67ba8f0b2694c8be755d9c5355b025f126ba1d
“叶田野” <642416071@qq.com>
2021-08-02 19:06:55 +0800

    设计模式 jvm

--- JVM/src/ClassLoaderDemo.java
+++ JVM/src/ClassLoaderDemo.java
@@ -3,5 +3,7 @@ public class ClassLoaderDemo {
         System.out.println("ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader());
         System.out.println("The Parent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent());
         System.out.println("The GrandParent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
+
+        System.out.println(213);
     }
 }
